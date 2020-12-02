7b8fcff93e47e19b19d8ba6432d7e5a541a22a2a
keonoh jeon <jko3546@gmail.com>
2020-12-02 18:17:12 +0900

    프레그먼트 목록화 방식에서 리사이클러뷰 방식으로 복귀

--- app/src/main/java/com/koko_plan/MainActivity.java
+++ app/src/main/java/com/koko_plan/MainActivity.java
@@ -12,13 +12,7 @@ import androidx.room.PrimaryKey;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-import android.animation.LayoutTransition;
-import android.annotation.SuppressLint;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -26,12 +20,10 @@ import android.graphics.Paint;
 import android.graphics.RectF;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -40,145 +32,134 @@ import com.melnykov.fab.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
     private EditText mTodoEditText;
-    private TextView tvTitle, mResultTextView, tvcycle, tvPlayTime;
+    private TextView mResultTextView, tvcycle, tvPlayTime;
 
+    private RecyclerAdapter adapter;
+    private RecyclerView recyclerView;
     private Paint p = new Paint();
-    private int size, oldsize;
-    public static MemoDatabase db;
-    private List<Todo> items = new ArrayList<>();
 
-    private SharedPreferences pref;
-    private static SharedPreferences.Editor editor;
-
-    ItemTouchHelper helper;
+    private MemoDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View btnPlus = findViewById(R.id.btnPlus);
 
-        LinearLayout container = (LinearLayout)findViewById(R.id.container);
-        container.setLayoutTransition(new LayoutTransition());
+        recyclerView = (RecyclerView) findViewById(R.id.rv_view);
 
-        pref = getSharedPreferences("pref", MODE_PRIVATE);
-        editor = pref.edit();
+        initSwipe();
+
+        btnPlus.setOnClickListener(v -> {
+            // title 입력 다이얼로그를 호출한다.
+            // title 입력하여 리사이클러뷰 addItem
+            final EditText edittext = new EditText(this);
+            edittext.setGravity(Gravity.CENTER);
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("일정 추가");
+            builder.setMessage("새로운 일정을 추가해주세요.");
+            builder.setView(edittext);
+
+            builder.setPositiveButton("입력",
+                    (dialog, which) -> {
+                        //제목 입력, DB추가
+                        if (!edittext.getText().toString().isEmpty()) {
+                            new Thread(() -> {
+                                Memo memo = new Memo(edittext.getText().toString(), null, 0, 0);
+                                db.memoDao().insert(memo);
+                            }).start();
+
+                        }
+
+                    });
+            builder.setNegativeButton("취소",
+                    (dialog, which) -> {
+                        //취소버튼 클릭
+                    });
+            builder.show();
+
+        });
+
 
         db = MemoDatabase.getDatabase(this);
 
-        /*//UI 갱신 (라이브데이터 Observer 이용, 해당 디비값이 변화가생기면 실행됨)
-        db.todoDao().getAll().observe(this, new Observer<List<Todo>>() {
-            @SuppressLint("ApplySharedPref")
-            @Override
-            public void onChanged(List<Todo> items) {
-                size = items.size();
-                editor.putInt("size", size);
-                editor.commit();
-
-                if (size == 0) {
-                    for (int i=0 ; i<size ; i++) {
-                        Fragment list = new MyTimerFragment();
-                        FragmentManager fm = getFragmentManager();
-                        FragmentTransaction t = fm.beginTransaction();
-                        //프레그먼트 매니저 상에 새 프레그먼트 추가
-                        t.add(R.id.container, list);
-                        t.commit();
-
-//            Log.d(TAG, "onChanged: size " + items.get(i));
-
-            *//*Bundle bundle = new Bundle();
-            bundle.putString("title", items.get(i).getTitle());
-            list.setArguments(bundle);*//*
-                    }
-                }
-            }
-        });*/
-
-        View btnPlus = findViewById(R.id.btnPlus);
-        btnPlus.setOnClickListener(this::buttonMethodAdd);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new RecyclerAdapter(db);
+        recyclerView.setAdapter(adapter);
 
         //UI 갱신 (라이브데이터 Observer 이용, 해당 디비값이 변화가생기면 실행됨)
-        db.todoDao().getAll().observe(this, new Observer<List<Todo>>() {
-            @SuppressLint("ApplySharedPref")
+        db.memoDao().getAll().observe(this, new Observer<List<Memo>>() {
             @Override
-            public void onChanged(List<Todo> items) {
-                size = items.size();
-                oldsize = pref.getInt("size", size);
-                if(oldsize == size) {
-                    for (int i=0 ; i<size ; i++) {
-                        Fragment list = new MyTimerFragment();
-                        FragmentManager fm = getFragmentManager();
-                        FragmentTransaction t = fm.beginTransaction();
-                        //프레그먼트 매니저 상에 새 프레그먼트 추가
-                        t.add(R.id.container, list);
-                        t.commit();
-
-                        Bundle bundle = new Bundle();
-                        bundle.putString("title", items.get(i).getTitle());
-                        list.setArguments(bundle);
-                    }
-                }
-                editor.putInt("size", size);
-                editor.commit();
+            public void onChanged(List<Memo> data) {
+                adapter.setItem(data);
             }
         });
     }
 
-    protected void onResume() {
-        super.onResume();
 
-    }
+    private void initSwipe() {
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT /* | ItemTouchHelper.RIGHT */) {
 
-    public void buttonMethodAdd(View v){
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
 
-        //일정 프레그먼트 추가 작업(텍스트 뷰 입력창 도입)
-        final EditText edittext = new EditText(this);
-        edittext.setGravity(Gravity.CENTER);
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("일정 추가");
-        builder.setMessage("새로운 일정을 추가해주세요.");
-        builder.setView(edittext);
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                int position = viewHolder.getAdapterPosition();
 
-        builder.setPositiveButton("입력",
-                (dialog, which) -> {
-                    //제목 입력, DB추가(빈칸이 아니면...)
-                    if (!edittext.getText().toString().isEmpty()) {
+                if (direction == ItemTouchHelper.LEFT) {
 
-                        //새로운 프레그 먼트 추가
-                        Fragment list = new MyTimerFragment();
-                        FragmentManager fm = getFragmentManager();
-                        FragmentTransaction t = fm.beginTransaction();
-                        //프레그먼트 매니저 상에 새 프레그먼트 추가
-                        t.add(R.id.container, list);
-                        t.commit();
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            db.memoDao().delete(adapter.getItems().get(position));
+                        }
 
-                        //번들에 저장하여, 신규 프레그먼트로 값 전달
-                        Bundle bundle = new Bundle();
-                        bundle.putString("title", edittext.getText().toString());
-                        list.setArguments(bundle);
+                    }).start();
+                }else {
+                    //오른쪽으로 밀었을때.
+                }
+            }
 
-                        //db에 신규일정 타이틀 저장
-                        new Thread(() -> {
-                            Todo memo = new Todo(0, edittext.getText().toString(), null, 0, 0);
-                            db.todoDao().insert(memo);
-                        }).start();
+            @Override
+            public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+
+                Bitmap icon;
+                if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE ) {
+
+                    View itemView = viewHolder.itemView;
+                    float height = (float) itemView.getBottom() - (float) itemView.getTop();
+                    float width = height / 3;
+
+                    if (dX > 0) {
+                        //오른쪽으로 밀었을 때
+
+                    } else {
+                        p.setColor(Color.parseColor("#D32F2F"));
+                        RectF background = new RectF((float) itemView.getRight() + dX, (float) itemView.getTop(), (float) itemView.getRight(), (float) itemView.getBottom());
+                        c.drawRect(background, p);
+                        /*
+                         * icon 추가할 수 있음.
+                         */
+                        //icon = BitmapFactory.decodeResource(getResources(), R.drawable.icon_png); //vector 불가!
+                        // RectF icon_dest = new RectF((float) itemView.getRight() - 2 * width, (float) itemView.getTop() + width, (float) itemView.getRight() - width, (float) itemView.getBottom() - width);
+                        //c.drawBitmap(icon, null, icon_dest, p);
                     }
-
-                });
-        builder.setNegativeButton("취소",
-                (dialog, which) -> {
-                    //취소버튼 클릭
-                });
-        builder.show();
+                }
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+        };
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        itemTouchHelper.attachToRecyclerView(recyclerView);
     }
+
 }
